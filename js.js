6bf39ae3dcaaf15adf7985beb18255221ab89f26
Nikolai2 <example@gmail.com>
2023-01-30 16:39:39 +0200

    30.01

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
-console.log('asd');
+const wrapper = document.querySelector('.wrapper');
+wrapper.addEventListener('click', (e) => {
+    // console.log(e.target);
+    if (e.target.closest('.h1', '.h2', '.h3')) {
+        console.log('asd');
+    }
+});
